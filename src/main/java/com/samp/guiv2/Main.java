8e7e0fb09453fb720d16b717e3177dc7b3c04fb1
Emmanuel Seyon <noyysey@gmail.com>
2022-08-19 20:26:46 -0500

    V6

--- src/main/java/com/samp/guiv2/Main.java
+++ src/main/java/com/samp/guiv2/Main.java
@@ -4,15 +4,41 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
        Parent root  = FXMLLoader.load(getClass().getResource("Main.fxml"));
-        stage.setTitle("Hello!");
-        stage.setScene(new Scene(root));
+
+      //Create Scenes
+        Scene scene = new Scene(root);
+        //Scene scene1 = new Scene(root);
+
+        //Add Image
+        Image lakpaLLC = new Image("xoxo.png" );
+        //Pass Image to Stage(Windows Label Pic)
+        stage.getIcons().add(lakpaLLC);
+
+        //Pass stylesheet (CSS) to scene
+        //scene.getStylesheets().add(getClass().getResource("App.css").toExternalForm());
+
+        //For multiple scenes
+        String css =this.getClass().getResource("App.css").toExternalForm();
+
+        scene.getStylesheets().add(css);
+        //scene1.getStylesheets().add(css);
+
+
+        //Pass Scene to stage
+        stage.setScene(scene);
+        //Set Titles
+        stage.setTitle("GUI2Demo!");
+
+
         stage.show();
+
     }
 
     public static void main(String[] args) {
