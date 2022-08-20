dbf331e302dea6575c299dd507508770d368e153
Emmanuel Seyon <noyysey@gmail.com>
2022-08-19 19:24:01 -0500

    V5

--- src/main/java/com/samp/guiv2/mainController.java
+++ src/main/java/com/samp/guiv2/mainController.java
@@ -4,20 +4,32 @@ import com.almasb.fxgl.entity.action.Action;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.shape.Circle;
 
+//Controller Class with methods for Functionality
 public class mainController {
-  public void UP(ActionEvent e){
-      System.out.println("UP");
+  //FXML Loader passes Values into Controller class
+    @FXML
+    private Circle myCircle;
+    private double x;
+    private double y;
+  //Methods For Action Events
+    public void UP(ActionEvent e){
+      //System.out.println("UP");
+        myCircle.setCenterY(y-=10);
   }
+    public void Down(ActionEvent e){
+        //System.out.println("Down");
+        myCircle.setCenterY(y+=10);
+    }
+    public void Left(ActionEvent e){
+        //System.out.println("Left");
+        myCircle.setCenterX(x-=10);
+    }
 
-    public void Down(ActionEvent e1){
-        System.out.println("Down");
-    }
-    public void Left(ActionEvent e2){
-        System.out.println("Left");
-    }
-    public void Right(ActionEvent e3){
-        System.out.println("Right");
+    public void Right(ActionEvent e){
+        //System.out.println("Right");
+        myCircle.setCenterX(x+=10);
     }
 
 
